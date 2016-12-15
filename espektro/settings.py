6f9b5c2d8100c82e8a96b9bb4139d8a98b0a85cb
Sumit Roy <sroy8091@gmail.com>
2016-12-15 23:40:14 +0530

    added mysql database

--- espektro/settings.py
+++ espektro/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'espektro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'espektro',
+        'USER':  'root',
+        'PASSWORD':  '713331',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
