15b3807167be617327ff61b4379588c733ea7e4f
Sumit Roy <sroy8091@gmail.com>
2016-12-17 12:07:08 +0530

    adding homepage

--- espektro/urls.py
+++ espektro/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
