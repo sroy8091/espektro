705e85eba5377d30716a0fb6d26783c81c22fca8
Sourajit Paul <sourajit.paul61@gmail.com>
2016-12-20 17:41:30 +0530

    adding campus ambassador in installed apps

--- espektro/settings.py
+++ espektro/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Campusambassador.apps.CampusambassadorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
