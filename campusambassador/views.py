257f9f5fd169cd9be176ec2d58cc728ee83648ce
Sourajit Paul <sourajit.paul61@gmail.com>
2016-12-20 17:37:16 +0530

    adding views for ambassador page..need changes

--- campusambassador/views.py
+++ campusambassador/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
+from .forms import SignupForm
+
+
+
+def get_name(request):
+    if request.method == 'POST' :
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('Thanks') #has to be changed according to design
+    else:
+        form = SignupForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'ambassador/forms.html' ,context)
