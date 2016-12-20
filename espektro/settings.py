4e815777990bd4c6c8d06ea6332074de5baf0d1f
Sourajit Paul <sourajit.paul61@gmail.com>
2016-12-20 17:44:24 +0530

    changing one mistake

--- espektro/settings.py
+++ espektro/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Campusambassador.apps.CampusambassadorConfig',
+    'campusambassador.apps.CampusambassadorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
