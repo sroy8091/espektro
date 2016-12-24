e6ce170644d46dd45b8a54f99c0e79340acc76fb
Krishna <krishagarwal.0493@gmail.com>
2016-12-25 01:25:36 +0530

    Adding events app

--- events/models.py
+++ events/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from stdimage import StdImageField
 
 # Create your models here.
+
+
+class Events(models.Model):
+	EventType_IN_CHOICES = (
+        ('Exotica', 'Exotica'),
+        ('Techtix', 'Techtix'),
+        )
+	EventType = models.CharField(
+        max_length=20, choices=EventType_IN_CHOICES, default='Exotica')
+
+	EventName = models.CharField(max_length=200)
+	EventImage = StdImageField(upload_to='event/event_image', variations={'thumbnail':(650,350, True)})
+	tagline = models.TextField(default='Rocking Tagline here', null=True)
+	EventDetails = models.TextField(null=True)
+	EventDate = models.DateField()
+	StartTime = models.TimeField(null =True)
+	EndTime = models.TimeField(null=True)
+	
+	def __str__(self):
+		return self.EventName
+
+class Event_Coordinator(models.Model):
+	CoordinatorName = models.CharField(max_length=200)
+	CoordinatorImage = StdImageField(upload_to='event/coordinator_image', variations={'thumbnail':(150, 120, True)})
+	CoordinatorEmail = models.EmailField()
+	# CoordinatorPhone = PhoneNumberField()
+	CoordinatorEvent = models.ForeignKey(Events)
+	
+
+	def __str__(self):
+		return self.CoordinatorName
