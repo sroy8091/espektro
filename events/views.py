447a9f4fc84c39f0847132dfee0c90479c0454ce
Krishna <krishagarwal.0493@gmail.com>
2016-12-25 01:26:15 +0530

    Writing the views for events app

--- events/views.py
+++ events/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from events.models import Events, Event_Coordinator
 
 # Create your views here.
+
+
+def event(request, event):
+
+    e_list = Event.objects.filter(EventName=event)
+   	eco_list=Event_Coordinator.objects.filter(EventName=event)
+    context = {'event': event, 'event_list': e_list, 'eventcoordinator_list':eco_list}
+    return render(request, 'events.html', context)
+
+
+def event_coordinator(request, id):
+
+    coordinator_info = Event_Coordinator.objects.filter(CoordinatorEvent=event)
+    context = {'c_info': coordinator_info}
+    return render(request, 'events.html', context)
