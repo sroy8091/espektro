b724805ed1e7a8213d420845d042b7806653cf2f
Krishna <krishagarwal.0493@gmail.com>
2016-12-25 01:25:01 +0530

    Registering the events aap in admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Events, Event_Coordinator
+
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('EventType', 'EventName', 'EventImage',
+                    'tagline', 'EventDetails', 'EventDate', 'StartTime','EndTime')
+admin.site.register(Events, EventsAdmin)
+
+class Event_CoordinatorAdmin(admin.ModelAdmin):
+    list_display = ('CoordinatorName', 'CoordinatorImage',
+                    'CoordinatorEmail', 'CoordinatorEvent')
+admin.site.register(Event_Coordinator, Event_CoordinatorAdmin)
+
+
+
+
+
