13971beade873e9b8acc425e21dde8c2715f0825
Biswajit Pain <bpain@vmware.com>
2017-02-19 18:37:54 +0530

    admin changedp added

--- changedp/admin.py
+++ changedp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Photos
-admin.site.register(Photos)
+from .models import  UploadedImage
+from .forms  import  UploadImageForm
+class UploadImageAdmin(admin.ModelAdmin):
+   list_display = ["Name","Email","Image","Filter"]
+   form = UploadImageForm 
+
+
+admin.site.register(UploadedImage, UploadImageAdmin)
