551aed55f917f59e5e620e4b2505fbf84cfe2a7a
hundredrab <jha.sourab@gmail.com>
2017-02-21 03:34:13 +0530

    Event app

---
+++ events/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+app_name='events'
+
+urlpatterns = [
+
+    # /techtix/
+    url(r'^$', views.techtix, name='techtix'),
+
+    # /addcontent/
+    url(r'^addcontent/$', views.addcontent),
+   
+]
