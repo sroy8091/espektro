836ea00d186d1346dac13a1d2fc3051df623a65e
Sumit Roy <sroy8091@gmail.com>
2017-02-21 19:41:15 +0530

    updating templates

--- profile/forms.py
+++ profile/forms.py
@@ -2,44 +2,48 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import UserDetail, Team
 
-class UserForm(forms.ModelForm):
-	password = forms.CharField(label='Password',widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
-	class Meta:
-		model = User
-		fields = ['username', 'first_name', 'last_name','email']
 
-	def clean_password2(self):
-		cd = self.cleaned_data
-		if cd['password'] != cd['password2']:
-			raise forms.ValidationError("Passwords do not match.")
-		return cd[passsword2]
+class UserForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords do not match.")
+        return cd['password2']
+
 
 class LoginForm(forms.Form):
-	username = forms.CharField(label='User Name', max_length=64)
-	password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(label='User Name', max_length=64)
+    password = forms.CharField(widget=forms.PasswordInput())
 
 
 class UserEditForm(forms.ModelForm):
-	
-	class Meta:
-		model = User
-		fields = ('first_name', 'last_name', 'email')
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
 
 
 class UserDetailEditForm(forms.ModelForm):
-	class Meta:
-		model = UserDetail
-		fields = ('college','city','phone_no','image','accomodation')
+    class Meta:
+        model = UserDetail
+        fields = ('college', 'city', 'phone_no', 'image', 'accomodation')
 
 
 class team_create_form(forms.ModelForm):
-	class Meta:
-		model = Team
-		fields = ('name', 'event')
+    class Meta:
+        model = Team
+        fields = ('name', 'event')
+
 
 class TeamInviteForm(forms.Form):
-	email = forms.EmailField()
+    email = forms.EmailField()
+
 
 class TeamAcceptForm(forms.Form):
-	user = forms.CharField()
+    user = forms.CharField()
