702625ca50363153569ad85a975f2f199b9d3d8e
Sumit Roy <sroy8091@gmail.com>
2017-02-22 11:18:00 +0530

    exotica and profile

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 from . import views
 
 app_name='events'
@@ -7,6 +9,11 @@ app_name='events'
 urlpatterns = [
 
     # /techtix/
-    url(r'^$', views.techtix, name='techtix'),
+    url(r'^techtix/$', views.techtix, name='techtix'),
+    url(r'^exotica/$', views.exotica, name='exotica'),
+
+    #exotica iframe
+    url(r'^testgame/', TemplateView.as_view(template_name="events/exotica_frame.html"),
+                   name='iframess'),
    
 ]
