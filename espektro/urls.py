683df618db04d4ae0952519cd9a80b7211e45fa4
Sumit Roy <sroy8091@gmail.com>
2017-02-23 23:47:48 +0530

    contribution page

--- espektro/urls.py
+++ espektro/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
     url(r'^changedp/', include('changedp.urls')),
     url(r'^events/', include('events.urls')),
     url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^webteam/$', TemplateView.as_view(template_name=''))
+    url(r'^webteam/$', TemplateView.as_view(template_name='webteam.html'), name='webteam')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
