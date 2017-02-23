8afc29f2d62472f84dcf14d4c3fe2d4a904c3b8a
riteshmukim <riteshmukim@gmail.com>
2017-02-23 22:49:19 +0530

    Team +minor chances

--- espektro/urls.py
+++ espektro/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     url(r'^changedp/', include('changedp.urls')),
     url(r'^events/', include('events.urls')),
     url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
-
+    url(r'^webteam/', TemplateView.as_view(template_name='webteam.html'), name='webteam'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
